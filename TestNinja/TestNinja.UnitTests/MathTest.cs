af6c6334e9035b68e4aee9dc2144c26ba8bf048b
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-16 16:40:30 +0700

    2_11 Writing Trustworthy Test

--- TestNinja/TestNinja.UnitTests/MathTest.cs
+++ TestNinja/TestNinja.UnitTests/MathTest.cs
@@ -17,13 +17,14 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
-        [Ignore("Because I wanted to!")]
+  //      [Ignore("Because I wanted to!")]
         public void Add_Whencalled_ReturntheSumOfArguments()
         {
 
             var result = _math.Max(1, 2);
 
-            Assert.That(result, Is.EqualTo(3));
+                    Assert.That(result, Is.EqualTo(3));
+           
         }
 
         [Test]
--- TestNinja/TestNinja/fundamentals/Math.cs
+++ TestNinja/TestNinja/fundamentals/Math.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace TestNinja.fundamentals
+namespace TestNinja.Fundamentals
 {
-    class Math
+    public class Math
     {
+        public int Add(int a, int b)
+        {
+
+            return a+b;
+        }
+
+        public int Max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
+        public IEnumerable<int> GetOddNumbers(int limit)
+        {
+            for (var i = 0; i <= limit; i++)
+                if (i % 2 != 0)
+                    yield return i;
+        }
     }
 }
