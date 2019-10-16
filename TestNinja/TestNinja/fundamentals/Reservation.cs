2743183281827130876bd4f03e9a3b30a5d26550
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-16 15:13:15 +0700

    1_9Refactoring with Confidence

--- TestNinja/TestNinja/fundamentals/Reservation.cs
+++ TestNinja/TestNinja/fundamentals/Reservation.cs
@@ -6,11 +6,7 @@
 
         public bool CanBeCancelledBy(User user)
         {
-            if (user.IsAdmin)
-                return false;
-            if (MadeBy == user)
-                return true;
-            return false;
+            return (user.IsAdmin || MadeBy == user);
         }
 
     }
