f41c5378c17f042005560707569c2eac4c7513a9
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-17 20:40:22 +0700

    5_3Refactoring Towards a Loosely-coupled Design

---
+++ TestNinja/TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,11 @@
+﻿namespace TestNinja.UnitTests
+{
+   public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+
+    }
+}
---
+++ TestNinja/TestNinja/Mocking/VideoService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TestNinja.Mocking
+{
+    public class VideoService
+    {
+       public string ReadVideoTitle()
+        {
+            var str = new FileReader().Read("video.txt");
+            var video = JsonConvert.DeserializeObject<Video>(str);
+            if (video == null)
+                return "Error parsing the video.";
+            return video.Title;
+        }
+
+        public string GetUnprocessedVideosAsCsv()
+        {
+            var videoIds = new List<int>();
+
+            using(var context = new VideoContext())
+
+            var videos = _repository.GetUnprocessedVideos();
+            foreach (var v in videos)
+                videoIds.Add(v.Id);
+
+            return String.Join(",", videoIds);
+        }
+    }
+
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsProcessed { get; set; }
+    }
+
+    public class VideoContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+}
