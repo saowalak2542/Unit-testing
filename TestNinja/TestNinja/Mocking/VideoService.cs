4af65733cd7d4ae6744c16fecf08ee3dd8a49920
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-18 15:52:24 +0700

    6_2Refactoring

--- TestNinja/TestNinja/Mocking/VideoService.cs
+++ TestNinja/TestNinja/Mocking/VideoService.cs
@@ -10,10 +10,12 @@ namespace TestNinja.Mocking
     public class VideoService
     {
         private IFileReader _fileReader;
+        private IVideoRepository _repository;
 
-        public VideoService(IFileReader fileReader )
+        public VideoService(IFileReader fileReader = null, IVideoRepository repository = null)
         {
-            _fileReader = fileReader;
+            _fileReader = fileReader ?? new FileReader();
+            _repository = repository ?? new VideoRepository();
         }
 
         public string ReadVideoTitle()
@@ -29,17 +31,24 @@ namespace TestNinja.Mocking
         {
             var videoIds = new List<int>();
 
-            using (var context = new VideoContext())
-            { 
-                var videos =
-                    (from Video in context.Videos
-                     where !video.IsProcessed
-                     select video).ToiList();
-
-            foreach (var v in videos)
+         var videos =  _repository.GetUnprocessedVideos();
+         foreach (var v in videos)
                 videoIds.Add(v.Id);
 
             return String.Join(",", videoIds);
+
         }
+    }
+
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsProcessed { get; set; }
+    }
+
+    public class VideoContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
     }
 }
