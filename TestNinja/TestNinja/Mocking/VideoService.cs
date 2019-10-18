19d3c3d3c22e9fa19de43885ccb7f6d7f58b9b5d
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-18 15:11:00 +0700

    6_1Exercise-VideoService

--- TestNinja/TestNinja/Mocking/VideoService.cs
+++ TestNinja/TestNinja/Mocking/VideoService.cs
@@ -30,24 +30,16 @@ namespace TestNinja.Mocking
             var videoIds = new List<int>();
 
             using (var context = new VideoContext())
+            { 
+                var videos =
+                    (from Video in context.Videos
+                     where !video.IsProcessed
+                     select video).ToiList();
 
-                var videos = _repository.GetUnprocessedVideos();
             foreach (var v in videos)
                 videoIds.Add(v.Id);
 
             return String.Join(",", videoIds);
         }
-    }
-
-    public class Video
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsProcessed { get; set; }
-    }
-
-    public class VideoContext : DbContext
-    {
-        public DbSet<Video> Videos { get; set; }
     }
 }
