caf7dc2dd1f0f435f3bc9640300dc50c8ce8e9ac
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-18 22:51:32 +0700

    6_7 Exercise-EmployeeHelper

---
+++ TestNinja/TestNinja/Mocking/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+
+namespace TestNinja.Mocking
+{
+    public class EmployeeController
+    {
+        private EmployeeContext _db;
+        public EmployeeController()
+        {
+            _db = new EmployeeContext();
+        }
+
+        public ActionResult DeleteEmployee(int id)
+        {
+            var employee = _db.Employees.Find(id);
+            _db.Employees.Remove(employee);
+            _db.SaveChanges();
+            return RedirectToAction("Employees");
+        }
+        private ActionResult RedirectToAction(string employees)
+        {
+            
+        }
+    }
+   
+}
