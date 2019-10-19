8ce7b7fd605b82bee1b8ccc70df693ac39fdf3c1
saowalak2542 <56579726+saowalak2542@users.noreply.github.com>
2019-10-19 16:04:34 +0700

    7_3Extracting IBooking Repository

---
+++ TestNinja/TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace TestNinja.Mocking
+{
+    public class BookingRepository
+    {
+      public IQueryable<Booking> GetAvtiveBookings(int? excludedBookingId = null)
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                .Where(
+                    b => b.Status != "Cancelled");
+
+            if(excludedBookingId.HasValue)
+                bookings = bookings.Where(b => b.Id != excludedBookingId.Value )
+
+
+            return bookings;
+        }
+    }
+}
